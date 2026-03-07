fef4c7f592c989eae50933cfe1cbed591ed5933e
Md Mahmudul Hossain <mahmudsazzad77@gmail.com>
2026-03-07 17:04:27 +0600

    Ui-Update

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function LoginPage() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  );
+  return <div>Login Page</div>;
 }
--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import NavMenu from "../Components/NavMenu";
+import FooterMenu from "../Components/FooterMenu";
+import image from "../assets/dp.jpeg";
 
 const MyProfile = () => {
   return (
     <div>
-      <h1>My profile</h1>
+      <NavMenu></NavMenu>
+      <div className="flex items-center justify-center gap-30 min-h-screen">
+        <div>
+          <figure>
+            <img width={200} src={image} alt="" />
+          </figure>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">Md Mahmudul Hossain</h1>
+          <p>Web Developer</p>
+        </div>
+      </div>
+      <FooterMenu></FooterMenu>
     </div>
   );
 };
