e42a3f160988ab272091e534ecd77cebfa927842
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-03-07 12:19:50 -1200

    Services Details Update

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NavMenu from "../Components/NavMenu";
 import FooterMenu from "../Components/FooterMenu";
 import { useLoaderData, useParams } from "react-router";
 
 const ServiceDetails = () => {
-  const Servicedata = useLoaderData();
+  const [service, setService] = useState({});
+  const servicedata = useLoaderData();
   const { id } = useParams();
   console.log(id);
   return (
     <div>
       <NavMenu></NavMenu>
-
+      <div>
+        const SingleServiceData =
+        {servicedata.find((singleService) => singleService.serviceId == id)}
+        setService(SingleServiceData)
+      </div>
+      {/* Modiul 51-5 */}
       <FooterMenu></FooterMenu>
     </div>
   );
